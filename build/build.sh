62ac84c31f6d54a345be9b456f7d6edbe135e272
anvaka <anvaka@gmail.com>
2012-10-02 02:14:36 -0700

    Fixed build script, added gitignore

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 rm -rf output
 node r.js -o build.js
-#node r.js -o cssIn=../css/styles.css out=output/css/styles.css
 
 cp ../index.html output/index.html
+mkdir -v output/css && cp -r ../css/* output/css/
+mkdir -v output/js/templates && cp -r ../js/templates/* output/js/templates/
+mkdir -v output/js/data && cp -r ../js/data/* output/js/data/
 sed -i '' 's/js\/lib\/require.js/http:\/\/requirejs.org\/docs\/release\/1.0.5\/minified\/require.js/g' output/index.html
